b7d5892231dce43f24a121f15f1bb4c039a12c8b
calloc134 <115209243+calloc134@users.noreply.github.com>
2025-04-17 14:01:29 +0900

    exportするコードを追加

--- common/src/lib.rs
+++ common/src/lib.rs
@@ -3,3 +3,13 @@ pub mod crypto_utils;
 pub mod error;
 pub mod ring;
 pub mod rsa;
+
+// Re-export for external use
+pub use constants::*;
+pub use crypto_utils::{d_k, e_k};
+pub use error::{RingError, RsaError};
+pub use ring::{ring_sign, ring_verify, RingSignature};
+pub use rsa::{
+    generate_keypair, load_public_key_from_pem, load_secret_key_from_pem, rsa_sign, rsa_verify,
+    KeyPair, PublicKey, SecretKey,
+};
