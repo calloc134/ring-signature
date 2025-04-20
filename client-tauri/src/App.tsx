9324d4245befb24bff80ffb392325fc9dacc09fd
calloc134 <115209243+calloc134@users.noreply.github.com>
2025-04-20 20:55:12 +0900

    wouterを使うよう変更

--- client-tauri/src/App.tsx
+++ client-tauri/src/App.tsx
@@ -1,30 +1,31 @@
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { Route, Switch, Link, Redirect } from "wouter";
 import SignPage from "./SignPage";
 import VerifyPage from "./VerifyPage";
 import { Toaster } from "react-hot-toast";
 import "./App.css";
 
-function App() {
-  return (
-    <BrowserRouter>
-      <div className="container mx-auto p-4">
-        <nav className="flex space-x-4 mb-4">
-          <Link to="/sign" className="text-blue-500 hover:underline">
-            Sign
-          </Link>
-          <Link to="/verify" className="text-blue-500 hover:underline">
-            Verify
-          </Link>
-        </nav>
-        <Routes>
-          <Route path="/sign" element={<SignPage />} />
-          <Route path="/verify" element={<VerifyPage />} />
-          <Route path="*" element={<SignPage />} />
-        </Routes>
-      </div>
+const App: React.FC = () => (
+  <>
+    <header className="flex gap-4 p-4 bg-gray-100">
+      <Link href="/" className="text-blue-500">
+        Sign
+      </Link>
+      <Link href="/verify" className="text-blue-500">
+        Verify
+      </Link>
+    </header>
+
+    <main className=" mx-auto p-4">
+      <Switch>
+        <Route path="/" component={SignPage} />
+        <Route path="/verify" component={VerifyPage} />
+        <Route>
+          <Redirect to="/" />
+        </Route>
+      </Switch>
       <Toaster position="top-right" />
-    </BrowserRouter>
-  );
-}
+    </main>
+  </>
+);
 
 export default App;
